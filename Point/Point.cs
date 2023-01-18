8d44a810014a80930974c51731c646e8a3d74271
bergm_69gfgdx <bergm_69gfgdx@DESKTOP-ELQO2CC>
2023-01-18 23:02:32 +0200

    Add project files.

---
+++ Point/Point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace point
+{
+    public class Point
+    {
+        public double X;
+        public double Y;
+        public Point()
+        {
+            this.X = 0.0;
+            this.Y = 0.0;
+        }
+
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public double GetXPoint()
+        {
+            return this.X;
+        }
+        public double GetYPoint()
+        {
+            return this.Y;
+        }
+        public void SetX(double x)
+        {
+            this.X = x;
+        }
+        public void SetY(double y)
+        {
+            this.Y = y;
+        }
+        public Point Mid(Point other)
+        {
+            Point M = new Point();
+            M.SetX((this.X + other.GetXPoint()) / 2);
+            M.SetY((this.Y + other.GetYPoint()) / 2);
+            return M;
+        }
+        public double Distance(Point other)
+        {
+            double dX2 = Math.Pow((this.X - other.X), 2);
+            double dY2 = Math.Pow((this.Y - other.Y), 2);
+            return (Math.Sqrt(dX2 + dY2));
+        }
+        public string Description()
+        {
+            return $"x= {this.X} y={this.Y} ";
+        }
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r1 = new Random();
+            Point p1 = new Point(r1.Next(-10, 11), r1.Next(-10, 11));
+            Point p2 = new Point();
+            p2.SetX(r1.Next(-10, 11));
+            p2.SetY(r1.Next(-10, 11));
+            Point p3 = p1.Mid(p2);
+            Console.WriteLine($"P1 is {p1.Description()}");
+            Console.WriteLine($"P2 is {p2.Description()}");
+            Console.WriteLine($"P3 is {p3.Description()}");
+            Console.WriteLine($"The Distance Between P2 and P3 is:{p2.Distance(p3)}");
+          
+            Console.ReadKey();
+        }
+    }
+}       
+
